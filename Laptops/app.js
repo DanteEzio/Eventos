cdb42f54c229fc2c7febeb9e72abbb2cfad45975
DanteEzio <quantum120@hotmail.com>
2022-06-24 18:45:01 -0500

    Se completo la programacion de app.js

--- Laptops/app.js
+++ Laptops/app.js
@@ -1,10 +1,73 @@
-import { laptops } from "./baseDeDatos.js";
-import { carritoDeCompras } from "./carrito.js";
+// import { data } from "./baseDeDatos.js";
+// import { carritoDeCompras } from "./carrito.js";
 
-const cargarProductos = (laptops) => {
+// const cargarProductos = (laptops) => {
+//   let cards = " ";
+
+//   laptops.forEach((item) => {
+//     cards = `
+// <div class="col">
+//         <div class="card">
+//           <a href="">
+//             <img
+//             src="${item.img}"
+//             class="card-img-top"
+//             alt="..."
+//             style="width: 100%; height: 250px"
+//           />
+//           </a>
+//           <div class="card-body">
+//             <a href="">
+//               <h5 class="card-title">
+//                 ${item.descripcion}
+//               </h5>
+//             </a>
+//             <p class="text-muted">${item.sku}</p>
+//             <h6><s>$${item.pReal.toLocaleString()} MXN</s></h6>
+//             <h6>$${item.pDescuento.toLocaleString()} MXN</h6>
+//             <p>Disponibles: ${item.stock}pzs.</p>
+//             <button type="button" class="btn btn-secondary agregarCarrito" id=boton${
+//               item.id
+//             }>
+//               Añadir al carrito <i class="fa-solid fa-cart-shopping"></i>
+//             </button>
+//           </div>
+//         </div>
+//  </div>
+//     `;
+//     document.querySelector("#contenedorLaptops").innerHTML += cards;
+
+//     const boton = document.getElementById(`boton${item.id}`);
+//     boton.addEventListener("click", () => {
+//       carritoDeCompras(item.id);
+//       alert(`Se agrego ${item.nombre} al carrito`);
+//     });
+//   });
+// };
+
+// cargarProductos(laptops);
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+});
+
+const fetchData = async () => {
+  try {
+    const res = await fetch("api.json");
+    const data = await res.json();
+    // console.log(data)
+    mostrarProductos(data);
+    detectarBotones(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const contenedorProductos = document.querySelector("#contenedorLaptops");
+const mostrarProductos = (data) => {
   let cards = " ";
 
-  laptops.forEach((item) => {
+  data.forEach((item) => {
     cards = `
       <div class="col">
               <div class="card">
@@ -33,16 +96,146 @@ const cargarProductos = (laptops) => {
                   </button>
                 </div>
               </div>
-       </div>
+      </div>
     `;
-    document.querySelector("#contenedorLaptops").innerHTML += cards;
+    contenedorProductos.innerHTML += cards;
+  });
+};
 
-    let boton = document.getElementById(`boton${item.id}`);
-    boton.addEventListener("click", ()=> {
-      carritoDeCompras(item.id);
-      alert(`Se agrego ${item.nombre} al carrito`);
+//Aqui creamos un carrito de objetos para que sea mas sencilla la manipulacion de las cantidades
+let carrito = {};
+
+const detectarBotones = (data) => {
+  const botones = document.querySelectorAll(".card-body button");
+
+  // console.log(botones)
+
+  botones.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      console.log(`Se agrego ${btn.id} al carrito`);
+      const producto = data.find((item) => `boton${item.id}` == btn.id);
+      // console.log(producto)
+
+      // Aquí estamos agregando el atributo cantidad, que sería la cantidad de productos que estaría comprando el usuario
+      producto.cantidad = 1;
+
+      //Aqui estamos haciendo nuestra condicion donde mencionamos que si ya existe el producto no lo duplique, únicamente incremente la cantidad
+      if (carrito.hasOwnProperty(producto.id)) {
+        // console.log("existe");
+        producto.cantidad = carrito[producto.id].cantidad + 1
+      }
+      //Indicamos su índice y agregamos los elemento del producto, en pocas palabras estamos reemplazando el elemento ya creado y solo se le agrega la cantidad inicializada
+      carrito[producto.id] = { ...producto };
+      // console.log(carrito);
+
+      mostrarCarrito();
     });
   });
 };
 
-cargarProductos(laptops);
+const pAgregados = document.querySelector("#pAgregados");
+
+const mostrarCarrito = () => {
+
+  pAgregados.innerHTML = " ";
+
+  const template = document.querySelector("#template-carrito").content;
+  const fragment = document.createDocumentFragment()
+
+  //Aqui estamos transformando nuestra lista de objetos en un ARRAY
+  Object.values(carrito).forEach((producto) => {
+    // console.log(producto)
+
+    template.querySelector(".card-img-top").setAttribute("src", producto.img);
+    template.querySelectorAll("td")[2].textContent = producto.cantidad;
+    template.querySelectorAll("td")[1].textContent = producto.nombre
+    template.querySelectorAll(
+      "td"
+    )[4].textContent = `$${(producto.pDescuento * producto.cantidad).toLocaleString()} MXN`;
+
+
+    //botones
+    template.querySelector(".incrementar").dataset.id = producto.id;
+    template.querySelector(".decrementar").dataset.id = producto.id;
+
+    const clone = template.cloneNode(true)
+    fragment.appendChild(clone)
+  });
+
+  pAgregados.appendChild(fragment);
+
+  mostrarFooterCarrito()
+  accionBotones()
+};
+
+const footerCarrito = document.querySelector("#footerCarrito");
+//Esta función nos muestra el footer del carrito (cantidad total y costo total)
+const mostrarFooterCarrito = () => {
+
+  footerCarrito.innerHTML = " "
+
+  if (Object.keys(carrito).length === 0) {
+    footerCarrito.innerHTML = `
+    <th scope="row" colspan="5">Carrito vacío</th>
+    `;
+
+    return
+  }
+
+  const template = document.querySelector("#templateFooterCarrito").content;
+  const fragment = document.createDocumentFragment();
+
+  //En este apartado necesitaremos sumar la cantidad total de los productos y el costo total
+  const cTotal = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
+  const pTotal = Object.values(carrito).reduce(
+    (acc, { cantidad, pDescuento }) => acc + cantidad * pDescuento,
+    0
+  );
+  // console.log(pTotal)
+
+  template.querySelectorAll("th")[1].textContent = cTotal;
+  template.querySelectorAll("th")[2].textContent = `$${pTotal.toLocaleString()} MXN`;
+
+  const clone = template.cloneNode(true);
+  fragment.appendChild(clone);
+
+  footerCarrito.appendChild(fragment);
+
+  const boton = document.querySelector("#vaciar-carrito");
+  boton.addEventListener("click", () => {
+    carrito = {}
+
+    mostrarCarrito()
+
+  })
+}
+
+accionBotones = () => {
+  const botonesAgregar = document.querySelectorAll(".incrementar");
+  const botonesEliminar = document.querySelectorAll(".decrementar");
+
+  botonesAgregar.forEach(btn => {
+    btn.addEventListener("click", () => {
+      // console.log("agregando...")
+      const producto = carrito[btn.dataset.id]
+      producto.cantidad ++
+      carrito[btn.dataset.id] = { ...producto }
+      mostrarCarrito()
+    })
+  })
+
+  botonesEliminar.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      // console.log("Eliminando...");
+      const producto = carrito[btn.dataset.id];
+      producto.cantidad--;
+      if (producto.cantidad === 0) {
+        delete carrito[btn.dataset.id];
+        mostrarCarrito();
+      } else {
+        carrito[btn.dataset.id] = { ...producto };
+        mostrarCarrito();
+      }
+    });
+  });
+}
--- Laptops/modal.js
+++ Laptops/modal.js
@@ -1,3 +1,5 @@
+// const modalContenedor = document.querySelector(".carritoCompras");
+
 const modalContainer = document.querySelector(".modalCarrito");
 
 const abrirCarrito = document.getElementById("open");
@@ -9,5 +11,13 @@ abrirCarrito.addEventListener("click", () => {
 });
 
 cerrarCarrito.addEventListener("click", () => {
-    modalContainer.classList.toggle("modal-active")
-})
+  modalContainer.classList.remove("modal-active");
+});
+
+// modalContenedor.addEventListener("click", () => {
+//   cerrarCarrito.click();
+// });
+
+modalContainer.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
